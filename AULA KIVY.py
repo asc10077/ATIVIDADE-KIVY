9fc22cb24762a01f3d5a5a7c103c22858f1c3f8d
asc10077 <165177045+asc10077@users.noreply.github.com>
2024-04-18 10:25:21 -0300

    Add files via upload

---
+++ AULA KIVY.py
@@ -0,0 +1,9 @@
+from kivy.app import App
+from kivy.uix.button import Button
+class MyApp(App):
+    def build (self):
+        return Button (text= "Hello world! This is my first program in kivy \n I'm a SESIANO student, and I love my teacher")
+if __name__ == "__main__":
+    MyApp().run()
+
+
